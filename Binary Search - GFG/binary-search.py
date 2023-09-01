b20f65b2ecd468ddca66fdf51af3872913aea8f0
Pavan Emani <pavanemani14@gmail.com>
2023-09-01 14:50:11 +0530

    Added solution - LeetHub

--- Binary Search - GFG/binary-search.py
+++ Binary Search - GFG/binary-search.py
@@ -3,17 +3,17 @@
 class Solution:	
 	def binarysearch(self, arr, n, k):
 		# code here
-		low=0
-		high=n-1
-		while low <= high:
-		    mid=low+(high-low)//2
-		    if arr[mid]==k:
-		        return mid
-	        elif arr[mid]<k:
-	            low=mid+1
-            else:
-                high=mid-1
-        return -1
+		i=0
+		j=n-1
+		while(i<=j):
+		    m=i+(j-i)//2
+		    if arr[m]==k:
+		        return m
+		    elif arr[m]>k:
+		        j=m-1
+	        else:
+	            i=m+1
+        return -1   
 
 
 #{ 
