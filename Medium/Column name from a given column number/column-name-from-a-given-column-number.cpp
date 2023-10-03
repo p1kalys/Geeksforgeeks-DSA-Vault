aaeaf4ce9c68cd23a0caa51ade1745677f7cb048
Pavan Emani <pavanemani14@gmail.com>
2023-10-03 09:43:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Column name from a given column number/column-name-from-a-given-column-number.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        // your code here
+        string ans = "";
+        while(n > 0){
+            n--;
+            char ch = (char) (n % 26 + 'A');
+            ans =   (ch) + ans;
+            n = n/ 26;
+        }
+            
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+
+// } Driver Code Ends
