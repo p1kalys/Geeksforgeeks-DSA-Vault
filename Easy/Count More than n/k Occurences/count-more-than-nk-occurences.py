f696e27b1449eca31bceb9d6b654120f73a8eec4
Pavan Emani <pavanemani14@gmail.com>
2023-12-23 18:57:17 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count More than n/k Occurences/count-more-than-nk-occurences.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function to find all elements in array that appear more than n/k times.
+    def countOccurence(self,arr,n,k):
+        #Your code here
+        res = 0
+        d = {}
+        for i in range(n):
+            d[arr[i]] = d.get(arr[i], 0)+1
+        for i,j in d.items():
+            if j > (n//k):
+                res += 1
+        return res
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            N=int(input())
+
+            A=list(map(int,input().split()))
+            
+            K=int(input())
+            
+            print(Solution().countOccurence(A, N, K))
+            
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
