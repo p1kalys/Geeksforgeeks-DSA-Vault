2e8659e3c5a9b8efc3f83b2278a45a5dfe5dc758
Pavan Emani <pavanemani14@gmail.com>
2024-01-27 09:31:11 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Stock buy and sell/stock-buy-and-sell.py
@@ -0,0 +1,51 @@
+#User function template for Python
+
+class Solution:
+    #Function to find the days of buying and selling stock for max profit.
+	def stockBuySell(self, A, n):
+		#code here
+		buy = A[0]
+		buy_index = 0
+		res = []
+		for i in range(1,n):
+		    if buy > A[i]:
+		        buy = A[i]
+		        buy_index = i
+		    else:
+		        res.append([buy_index, i])
+		        buy_index = i
+		        buy = A[i]
+	    return res
+		        
+
+
+#{ 
+ # Driver Code Starts
+#Initial template for Python
+
+def check(ans,A,p):
+    c = 0
+    for i in range(len(ans)):
+        c += A[ans[i][1]]-A[ans[i][0]]
+    if(c==p):
+        return 1 
+    else:
+        return 0
+
+if __name__=='__main__':
+	t = int(input())
+	while(t>0):
+		n = int(input())
+		A = [int(x) for x in input().strip().split()]
+		ob = Solution()
+		ans = ob.stockBuySell(A,n)
+		p=0
+		for i in range(n-1):
+		    p += max(0,A[i+1]-A[i])
+		if(len(ans) == 0):
+			print("No Profit",end="")
+		else:
+			print(check(ans,A,p),end="")
+		print()
+		t-=1
+# } Driver Code Ends
