52a8d34661126ec3b6717a8ab989bf8bf48e9f66
Pavan Emani <pavanemani14@gmail.com>
2024-01-31 12:45:41 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Insert and Search in a Trie/insert-and-search-in-a-trie.py
@@ -0,0 +1,72 @@
+#User function Template for python3
+
+"""
+class TrieNode: 
+      
+    def __init__(self): 
+        self.children = [None]*26
+  
+        # isEndOfWord is True if node represent the end of the word 
+        self.isEndOfWord = False
+"""
+
+class Solution:
+    #Function to insert string into TRIE.
+    def insert(self, root, key):
+        for j in key:
+            i = ord(j) - ord('a')
+            if not root.children[i]:
+                root.children[i] = TrieNode()
+            root = root.children[i]
+        root.isEndOfWord = True
+        #code here
+    
+    #Function to use TRIE data structure and search the given string.
+    def search(self, root, key):
+        for j in key:
+            i = ord(j) - ord('a')
+            if not root.children[i]:
+                return False
+            root = root.children[i]
+        return root.isEndOfWord
+        #code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+class TrieNode: 
+      
+    def __init__(self): 
+        self.children = [None]*26
+  
+        # isEndOfWord is True if node represent the end of the word 
+        self.isEndOfWord = False
+  
+class Trie: 
+      
+    # Trie data structure class 
+    def __init__(self): 
+        self.root =TrieNode()
+        
+
+if __name__ == '__main__': 
+    t=int(input())
+    for tcs in range(t):
+        n=int(input())
+        arr=input().strip().split()
+        strs=input()
+        
+        t=Trie()
+        ob = Solution()
+        
+        for s in arr:
+            ob.insert(t.root,s)
+        
+        if ob.search(t.root,strs):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
